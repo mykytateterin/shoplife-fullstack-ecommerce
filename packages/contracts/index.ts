efac44662aac825d71e6389ac57c2dbf28636035
Mykyta Teterin <78042683+mykytateterin@users.noreply.github.com>
2026-06-20 18:59:57 +0200

    feat(contracts): add user role contract

--- packages/contracts/index.ts
+++ packages/contracts/index.ts
@@ -1,1 +1,2 @@
+export * from './src/users/index.js';
 export * from './src/validation/index.js';
---
+++ packages/contracts/src/users/user-role.ts
@@ -0,0 +1,7 @@
+export const UserRole = {
+  ADMIN: 'ADMIN',
+  MANAGER: 'MANAGER',
+  USER: 'USER',
+} as const;
+
+export type UserRole = (typeof UserRole)[keyof typeof UserRole];
