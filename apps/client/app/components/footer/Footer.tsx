a5084f6e33dbba129848ef52034cb7a30a4dbaff
Mykyta Teterin <78042683+mykytateterin@users.noreply.github.com>
2026-07-08 19:00:51 +0200

    refactor(client): type footer component

--- apps/client/app/components/footer/Footer.tsx
+++ apps/client/app/components/footer/Footer.tsx
@@ -1,13 +1,15 @@
-import styles from './Footer.module.scss';
 import logo from '../../logo.svg';
+import styles from './Footer.module.scss';
 
-export const Footer = () => {
+const Footer = (): React.JSX.Element => {
   return (
-    <footer className={styles['footer']}>
-      <div className={styles['footer__body']}>
-        <img src={logo} alt="ShopLife logo" className={styles['footer__logo']} />
-        <p className={styles['footer__info']}>Made with ❤️ by Mykyta Teterin</p>
+    <footer className={styles.footer}>
+      <div className={styles.footer__body}>
+        <img alt="ShopLife logo" className={styles.footer__logo} src={logo} />
+        <p className={styles.footer__info}>Made with ❤️ by Mykyta Teterin</p>
       </div>
     </footer>
   );
 };
+
+export { Footer };
