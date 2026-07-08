7736dd34907c24ed604ee53bc38f458491dc67e9
Mykyta Teterin <78042683+mykytateterin@users.noreply.github.com>
2026-07-08 20:26:22 +0200

    refactor(client): type account page

--- apps/client/app/pages/account/AccountPage.tsx
+++ apps/client/app/pages/account/AccountPage.tsx
@@ -2,7 +2,7 @@ import { AuthForm, Profile } from '../../components';
 import { useAuthStore } from '../../modules/auth/auth.store';
 import styles from './AccountPage.module.scss';
 
-export const AccountPage = () => {
+const AccountPage = (): React.JSX.Element => {
   const user = useAuthStore((state) => state.user);
   const isSessionLoading = useAuthStore((state) => state.isSessionLoading);
 
@@ -12,3 +12,5 @@ export const AccountPage = () => {
 
   return <main className={styles['account-page']}>{user ? <Profile /> : <AuthForm />}</main>;
 };
+
+export { AccountPage };
