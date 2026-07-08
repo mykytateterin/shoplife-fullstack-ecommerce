b30c13aa936ca1b5f5c085f6d74ff74cc07aef7c
Mykyta Teterin <78042683+mykytateterin@users.noreply.github.com>
2026-07-08 20:28:58 +0200

    refactor(client): type not found page

--- apps/client/app/pages/not-found/NotFoundPage.tsx
+++ apps/client/app/pages/not-found/NotFoundPage.tsx
@@ -1,18 +1,19 @@
 import errorIcon from '../../assets/images/icons/error-page-icon.svg';
-
 import styles from './NotFoundPage.module.scss';
 
-export const NotFoundPage = () => {
+const NotFoundPage = (): React.JSX.Element => {
   return (
     <main className={styles['error-page']}>
       <div className={styles['error-page__body']}>
         <h1 className={styles['error-page__title']}>Page not found...</h1>
         <img
-          src={errorIcon}
           alt="Error 404 icon (page not found)"
           className={styles['error-page__image']}
+          src={errorIcon}
         />
       </div>
     </main>
   );
 };
+
+export { NotFoundPage };
