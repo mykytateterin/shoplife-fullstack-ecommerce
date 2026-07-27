2d2256200b92b35d71070f9d1b83d6e7a56917bd
Mykyta Teterin <78042683+mykytateterin@users.noreply.github.com>
2026-07-27 19:40:47 +0200

    refactor(client/not-found): rename CSS class, convert px to rem, simplify width

--- apps/client/app/pages/not-found/NotFoundPage.tsx
+++ apps/client/app/pages/not-found/NotFoundPage.tsx
@@ -3,12 +3,12 @@ import styles from './NotFoundPage.module.scss';
 
 const NotFoundPage = (): React.JSX.Element => {
   return (
-    <main className={styles['error-page']}>
-      <div className={styles['error-page__body']}>
-        <h1 className={styles['error-page__title']}>Page not found...</h1>
+    <main className={styles['not-found-page']}>
+      <div className={styles['not-found-page__body']}>
+        <h1 className={styles['not-found-page__title']}>Page not found...</h1>
         <img
           alt="Error 404 icon (page not found)"
-          className={styles['error-page__image']}
+          className={styles['not-found-page__image']}
           src={errorIcon}
         />
       </div>
